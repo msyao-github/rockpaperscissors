0f2bf3d3b2415b37d4f4224881956837314928b3
iTravelsky <itravelsky@iTravelskys-MBP.fios-router.home>
2016-01-14 14:49:28 -0500

    Finalize for Unit 6 HW Submission

--- rockpaperscissors.js
+++ rockpaperscissors.js
@@ -18,7 +18,7 @@ function randomPlay() {
     }
 }
 ////////////////////////////////////////////////
-/*           YaoWen Your Code Below            */
+/*              YW Code Below                 */
 ////////////////////////////////////////////////
 
 function getPlayerMove(move) {
@@ -53,26 +53,58 @@ function getWinner(playerMove,computerMove) {
        winner='Computer';
     }  else if (playerMove === 'scissors' && computerMove === 'rock') {
        winner='Computer';
-    }  else if (playerMove===computerMove){winner="tie";
-    }
+    }  else {winner="tie";
+    }  
+
+    return winner;
 }
 
 function playToFive() {
     console.log("Let's play Rock, Paper, Scissors");
     var playerWins = 0;
     var computerWins = 0;
-    // Write code that plays 'Rock, Paper, Scissors' until either the player or the computer has won five times.
-    /* YaoWen CODE HERE */
+    var playerMove = getPlayerMove();
+    var computerMove = getComputerMove();
+    var winner = getWinner(getPlayerMove, getComputerMove);
+
+  // This function should continue to play Rock Paper Scissors until either the
+  // player or the computer has won five times.
+  // After each 'round', display some text in the console indicating who played
+  // what, who won, and what the current scoreboard looks like.
+  // For example,
+  //  console.log('Player chose ' + playerMove + ' while Computer chose ' + computerMove);
+  //  console.log('The score is currently ' + playerWins + ' to ' + computerWins + '\n');
+  /* YOUR CODE HERE */
+  console.log('playerMove is ' + playerMove);
+  console.log('computerMove is ' + computerMove);
+  console.log('winner is ' + winner);
+
+  while ((playerWins < 5) || (computerWins < 5)) {
+    if (winner === "player") {
+        playerWins += 1; 
+    } 
+    else if (winner === "computer") {
+        computerWins += 1;
+    } 
+    else {
+        computerWins += 0;
+        playerWins+= 0;
+    }
+    
 
 
-
-
-
-
-
-
-
-
+    console.log('Player chose ' + playerMove + ' while Computer chose ' + computerMove);
+    
+    console.log('The score is currently ' + playerWins + ' to ' + computerWins + '\n');
+    
+    console.log('Result is ' + winner);
     return [playerWins, computerWins];
+    
+
+  }
+
 }
+
+ 
+playToFive();
 
